64d28d43e3c057cf341004b7946efacf74e9b8e4
kanaxx <36564954+kanaxx@users.noreply.github.com>
2020-08-22 16:03:28 +0900

    Create canonical_exported.js

---
+++ bookmarklet/canonical_exported.js
@@ -0,0 +1,19 @@
+export bookmarklet () => {
+  let title = document.title;
+  let href = document.location.href;
+  let canonical = get_canonical();
+  console.log('ページタイトル=%s \n href=%s \n カノニカルURL=%s', title, href, canonical);
+  alert('ページタイトル：'+title +"\n現在のURL："+href + "\nカノニカルURL：" + canonical);
+  
+  function get_canonical(){
+    links = document.getElementsByTagName("link");
+    for ( i in links) {
+      if (links[i].rel) {
+        if (links[i].rel.toLowerCase() == "canonical") {
+          return links[i].href;
+        }
+      }
+    }
+    return "";
+  }
+}
