8da45dd02a778a1504931c9c395bcf7fbe875d17
kanaxx <36564954+kanaxx@users.noreply.github.com>
2020-08-22 16:15:11 +0900

    Update canonical_exported.js

--- bookmarklet/canonical_exported.js
+++ bookmarklet/canonical_exported.js
@@ -1,19 +1,19 @@
-export default () => {
-  let title = document.title;
-  let href = document.location.href;
-  let canonical = get_canonical();
-  console.log('ページタイトル=%s \n href=%s \n カノニカルURL=%s', title, href, canonical);
-  alert('ページタイトル：'+title +"\n現在のURL："+href + "\nカノニカルURL：" + canonical);
-  
-  function get_canonical(){
-    links = document.getElementsByTagName("link");
-    for ( i in links) {
-      if (links[i].rel) {
-        if (links[i].rel.toLowerCase() == "canonical") {
-          return links[i].href;
+export default () => {  
+    let title = document.title;
+    let href = document.location.href;
+    let canonical = get_canonical();
+    console.log('ページタイトル=%s \n href=%s \n カノニカルURL=%s', title, href, canonical);
+    alert('ページタイトル：'+title +"\n現在のURL："+href + "\nカノニカルURL：" + canonical);
+    
+    function get_canonical(){
+      let links = document.getElementsByTagName("link");
+      for ( let i in links) {
+        if (links[i].rel) {
+          if (links[i].rel.toLowerCase() == "canonical") {
+            return links[i].href;
+          }
         }
       }
+      return "";
     }
-    return "";
-  }
 }
